064edc7d595311a81cf0e1e88a476af6c575f0f4
yulioaj290 <yulioaj290@gmail.com>
2017-04-08 11:55:06 -0400

    Initial commit.`

---
+++ smart-shopify-product.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Plugin Name: Smart Shopify Product
+ * Plugin URI: http://www.com
+ * Description: This plugin allows add some shortcodes on post to insert Shopify products. Also allow to retrieve automatically all products on the shopify store.
+ * Version: 1.0.0
+ * Author: Yulio Aleman Jimenez
+ * Author URI: http://yulioaj290.com
+ * License: GPL2
+ */ 
+
+

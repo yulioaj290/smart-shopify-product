e6afb26c8c501bcab04da1feb5e8fdd150d2e1c7
Yulio Aleman Jimenez <yulioaj290@gmail.com>
2017-04-14 11:29:14 -0400

    Added some parameters to checkout link.

--- public/js/shopify-scripts.js
+++ public/js/shopify-scripts.js
@@ -405,7 +405,7 @@ function bindEventListeners(product, is_product_page = false) {
 
 			/* checkout button click listener */
 			$('.btn--cart-checkout').on('click', function () {
-				window.open(cart.checkoutUrl, '_blank'/*, 'location=yes,height=570,width=520,scrollbars=yes,status=yes'*/);
+				window.open(cart.checkoutUrl, '_blank', 'location=yes,height=570,width=520,scrollbars=yes,status=yes');
 			});
 
 			/* increment quantity click listener */
